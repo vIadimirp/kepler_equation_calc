89b7ad44f825a6e6471118118e20dc9a77cc1ca1
Vladimir <118995973+vIadimirp@users.noreply.github.com>
2025-02-14 20:46:46 +0300

    Update kepler_equation.cpp

--- kepler_equation.cpp
+++ kepler_equation.cpp
@@ -23,7 +23,6 @@ int main() {
     unsigned short precision = 16;  // Floating point length
 
     // This is my simple binary search algorithm
-    // It's slightly modified because I dont know how to normally integrate it
     double E;
     do {
         E = (start + end) / 2;
